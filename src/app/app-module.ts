45a821666d45df6de934d630fca7010ee1509669
AnitaGeovanaBR <anita.geovana.br@gmail.com>
2025-12-17 09:49:40 -0300

    Adicionando componentes básicos.

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Inicio } from './paginas/inicio/inicio';
+import { MenuComponent } from './components/menu/menu';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Inicio,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Inicio } from './paginas/inicio/inicio';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: Inicio,
+    path: ''
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
